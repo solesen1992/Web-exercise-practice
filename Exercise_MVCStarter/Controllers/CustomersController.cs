a2a20011a882b765ea4bbc24d2240975f2a691fa
solesen1992 <saramundal@live.dk>
2024-10-17 12:35:15 +0200

    Add project files.

---
+++ Exercise_MVCStarter/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using Exercise_MVCStarter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise_MVCStarter.Controllers
+{
+    public class CustomersController : Controller
+    {
+        /*
+         * In the Index action you must hard code a list of Customer objects 
+         * and use the list as an argument in the call to the view.
+         */
+        public IActionResult Index()
+        {
+            // Hard-coded list of customers
+            var customers = new List<Customer>
+            {
+                new Customer(1, "John", "Doe"),
+                new Customer(2, "Jane", "Smith"),
+                new Customer(3, "Alice", "Johnson")
+            };
+
+            // Pass the list to the view
+            return View(customers);
+        }
+    }
+}
---
+++ Exercise_MVCStarter/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_MVCStarter.Models
+{
+    public class Customer
+    {
+        // Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Constructor
+        public Customer(int id, string firstName, string lastName)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
